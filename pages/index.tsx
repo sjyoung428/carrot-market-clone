dbb846644f034e588fff588e2495785105a7422a
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-06-26 14:32:35 +0900

    Chore: tsconfig에서 baseurl 및 alias 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <div className="bg-slate-600">
-        <h1 className="text-neutral-200">tailwindcss</h1>
-      </div>
-    </>
-  );
+  return <>null</>;
 };
 
 export default Home;
