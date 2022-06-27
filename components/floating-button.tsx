b67b419f0a3fac72465f6a43b3a2dd575f6ca2c3
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-06-27 18:42:36 +0900

    Fix: floating-button 찌그러지는 현상 수정

--- components/floating-button.tsx
+++ components/floating-button.tsx
@@ -9,7 +9,7 @@ interface IFloating {
 export default function FloatingButton({ children, href }: IFloating) {
   return (
     <Link href={href}>
-      <a className="fixed bottom-24 right-5 flex aspect-square w-14 cursor-pointer  items-center justify-center rounded-full border-0 border-transparent bg-orange-400 text-white shadow-xl transition-colors hover:bg-orange-500">
+      <a className="fixed bottom-24 right-5 flex aspect-square h-14 w-14 cursor-pointer  items-center justify-center rounded-full border-0 border-transparent bg-orange-400 text-white shadow-xl transition-colors hover:bg-orange-500">
         {children}
       </a>
     </Link>
