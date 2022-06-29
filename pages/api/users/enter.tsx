4c38733c4c80450a6a27179ce76bd8d89bdab4e1
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-06-29 11:17:16 +0900

    Feat: 토큰 생성 및 로직 변경

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,60 +6,33 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") res.status(401).end();
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  // const user = await client.user.upsert({
+  //   where: {
+  //     ...payload,
+  //   },
+  //   create: {
+  //     name: "익명",
+  //     ...payload,
+  //   },
+  //   update: {},
+  // });
+  const token = await client.token.create({
+    data: {
+      payload: "123", //랜덤
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "익명",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "익명",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
-  // let user;
-  // if (email) {
-  //   console.log("유저 찾는 중");
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email,
-  //     },
-  //   });
-
-  //   if (user) {
-  //     console.log("유저 찾음");
-  //   }
-  //   if (!user) {
-  //     console.log("회원가입함");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "익명",
-  //         email,
-  //       },
-  //     });
-  //   }
-  // }
-  // if (phone) {
-  //   console.log("유저 찾는 중");
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone: +phone,
-  //     },
-  //   });
-
-  //   if (user) {
-  //     console.log("유저 찾음");
-  //   }
-  //   if (!user) {
-  //     console.log("회원가입함");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "익명",
-  //         phone: +phone,
-  //       },
-  //     });
-  //   }
-  // }
+  console.log(token);
 
   return res.status(200).end();
 };
