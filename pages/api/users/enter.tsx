0bfb95443a17b2eaf9060e90cc44fae68fbfcf89
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-06-29 10:27:33 +0900

    Refactor: Account Logic with upsert

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,49 +5,61 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") res.status(401).end();
   const { phone, email } = req.body;
-  let user;
-  if (email) {
-    console.log("유저 찾는 중");
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "익명",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+  // let user;
+  // if (email) {
+  //   console.log("유저 찾는 중");
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
 
-    if (user) {
-      console.log("유저 찾음");
-    }
-    if (!user) {
-      console.log("회원가입함");
-      user = await client.user.create({
-        data: {
-          name: "익명",
-          email,
-        },
-      });
-    }
-  }
-  if (phone) {
-    console.log("유저 찾는 중");
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
+  //   if (user) {
+  //     console.log("유저 찾음");
+  //   }
+  //   if (!user) {
+  //     console.log("회원가입함");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "익명",
+  //         email,
+  //       },
+  //     });
+  //   }
+  // }
+  // if (phone) {
+  //   console.log("유저 찾는 중");
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
 
-    if (user) {
-      console.log("유저 찾음");
-    }
-    if (!user) {
-      console.log("회원가입함");
-      user = await client.user.create({
-        data: {
-          name: "익명",
-          phone: +phone,
-        },
-      });
-    }
-  }
+  //   if (user) {
+  //     console.log("유저 찾음");
+  //   }
+  //   if (!user) {
+  //     console.log("회원가입함");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "익명",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  // }
 
   return res.status(200).end();
 };
