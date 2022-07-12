e3d355f155b3669c20b0a507f6ce331420cd122f
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-07-12 18:14:56 +0900

    Feat: edit profile front

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,11 +2,42 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
+import useUser from "@libs/client/hooks/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+
+  const onValid = ({ email, phone }: EditProfileForm) => {
+    if (email === "" && phone === "") {
+      setError("formErrors", {
+        message: "이메일 또는 전화번호가 필수입니다.",
+      });
+    }
+  };
+
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="space-y-4 py-10 px-4">
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 py-10 px-4">
         <div className="flex items-center space-x-3">
           <div className="h-14 w-14 rounded-full bg-slate-500" />
           <label
@@ -22,14 +53,26 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
         <Input
-          required
+          register={register("email")}
+          required={false}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register("phone")}
+          required={false}
           label="Phone number"
           name="phone"
           type="number"
           kind="phone"
         />
+        {errors.formErrors ? (
+          <span className="mt-2 block text-center font-bold text-red-500 ">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
         <Button text="Update profile" />
       </form>
     </Layout>
