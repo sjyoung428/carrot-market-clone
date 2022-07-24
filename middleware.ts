cbcb2155f6c8f42dfee2d2924b25dec355c08286
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-07-24 19:23:51 +0900

    Feat: 미들웨어로 리다이렉트 로직 구현

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,31 @@
-import { NextRequest, userAgent } from "next/server";
-// import { NextResponse } from "next/server";
+import { NextResponse, userAgent } from "next/server";
+import type { NextRequest } from "next/server";
 
 export const middleware = (req: NextRequest) => {
-  console.log(userAgent(req));
+  const { isBot } = userAgent(req);
+  let ua = userAgent(req);
+  if (isBot) {
+    console.log(ua);
+    return NextResponse.rewrite(new URL("/enter", req.url));
+  }
+  let cookie = req.cookies.get("carrotsession");
+  console.log(cookie);
+  // console.log(req.cookies.get("carrotsession"));
+  if (!req.nextUrl.pathname.startsWith("/api")) {
+    if (!req.nextUrl.pathname.startsWith("/enter")) {
+      if (!cookie) {
+        req.nextUrl.pathname = "/enter";
+        return NextResponse.redirect(req.nextUrl);
+      }
+    }
+  }
+};
+export const config = {
+  matcher: [
+    "/",
+    "/enter",
+    "/products/:path*",
+    "/streams/:path*",
+    "/profile/:path*",
+  ],
 };
